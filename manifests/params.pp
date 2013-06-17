50419f5aa325d41392918e387768272aa1d243bd
Michael G. Noll <michael@michael-noll.com>
2013-06-17 17:57:14 +0200

    Patch: add a workaround so we can run supervisord 3.x on RHEL/CentOS 6
    The supervisord RPM was manually created by miguno on RHEL 6.

--- manifests/params.pp
+++ manifests/params.pp
@@ -10,10 +10,23 @@ class supervisor::params {
       $conf_file      = '/etc/supervisord.conf'
       $conf_dir       = '/etc/supervisord.d'
       $system_service = 'supervisord'
-      $package        = 'supervisor'
+      $package        = 'supervisor3'
     }
     default: {
       fail("Unsupported platform: ${::operatingsystem}")
     }
   }
+
+  ### miguno: DIRTY HACK to allow us to run supervisord 3.x on RHEL/CentOS 6
+  package { "supervisor3":
+    ensure   => latest,
+    source   => "https://s3.amazonaws.com/cdn.michael-noll.com/rpms/supervisor-3.0b2.el6.x86_64.rpm",
+    provider => rpm,
+    require  => Package["python-setuptools"],
+  }
+
+  package { "python-setuptools":
+    ensure   => latest,
+  }
+
 }
