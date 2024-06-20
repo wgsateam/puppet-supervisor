6c41b7c88bcf079cd773645c2e5b2bbd873b36ad
Andrey Burmenko <a_burmenko@wargaming.net>
2024-06-20 11:57:15 +0300

    allow change supervisor::params::conf_dir form hiera

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,19 +1,20 @@
 class supervisor::params {
   case $::operatingsystem {
     'ubuntu','debian': {
-      $conf_file      = '/etc/supervisor/supervisord.conf'
-      $conf_dir       = '/etc/supervisor/conf.d'
-      $system_service = 'supervisor'
-      $package        = 'supervisor'
+      $conf_file        = '/etc/supervisor/supervisord.conf'
+      $conf_dir_default = '/etc/supervisor/conf.d'
+      $system_service   = 'supervisor'
+      $package          = 'supervisor'
     }
     'centos','fedora','redhat','Amazon', 'OracleLinux': {
-      $conf_file      = '/etc/supervisord.conf'
-      $conf_dir       = '/etc/supervisord.d'
-      $system_service = 'supervisord'
-      $package        = 'supervisor'
+      $conf_file        = '/etc/supervisord.conf'
+      $conf_dir_default = '/etc/supervisord.d'
+      $system_service   = 'supervisord'
+      $package          = 'supervisor'
     }
     default: {
       fail("Unsupported platform: ${::operatingsystem}")
     }
   }
+  $conf_dir = lookup('supervisor::params::conf_dir', String, 'first', $conf_dir_default)
 }
