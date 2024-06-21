71c3214f19a6dfb33e1451ae44910445f5b46c98
Andrey Burmenko <a_burmenko@wargaming.net>
2024-06-21 15:51:46 +0300

    allow change supervisor::params::conf_dir form hiera. #2

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,20 +1,25 @@
 class supervisor::params {
   case $::operatingsystem {
     'ubuntu','debian': {
-      $conf_file        = '/etc/supervisor/supervisord.conf'
-      $conf_dir_default = '/etc/supervisor/conf.d'
-      $system_service   = 'supervisor'
-      $package          = 'supervisor'
+      $conf_file      = '/etc/supervisor/supervisord.conf'
+      $conf_dir       = '/etc/supervisor/conf.d'
+      $system_service = 'supervisor'
+      $package        = 'supervisor'
     }
     'centos','fedora','redhat','Amazon', 'OracleLinux': {
-      $conf_file        = '/etc/supervisord.conf'
-      $conf_dir_default = '/etc/supervisord.d'
-      $system_service   = 'supervisord'
-      $package          = 'supervisor'
+      $conf_file      = '/etc/supervisord.conf'
+      $conf_dir       = '/etc/supervisord.d'
+      $system_service = 'supervisord'
+      $package        = 'supervisor'
     }
     default: {
       fail("Unsupported platform: ${::operatingsystem}")
     }
   }
-  $conf_dir = lookup('supervisor::params::conf_dir', String, 'first', $conf_dir_default)
+  $conf_dir_extra = lookup('supervisor::params::conf_dir', String, 'first', '')
+  if $conf_dir_extra.empty() {
+    $conf_dir_str = regsubst("${conf_dir}/*.conf", '//', '/')
+  } else {
+    $conf_dir_str = regsubst("${conf_dir}/*.conf ${conf_dir_extra}/*.conf", '//', '/')
+  }
 }
